14bd9e3aad4b19b80b8cd28a54326a90f2b2dc81
Ally Glantzberg <allyglantzberg@gmail.com>
2025-06-05 15:29:35 -0400

    initial commit of editrunscript

---
+++ editrunscript.sh
@@ -0,0 +1,20 @@
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script01.b
+tail -2 run_script01.b
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script02.b
+tail -2 run_script02.b
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script03.b
+tail -2 run_script03.b
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script04.b
+tail -2 run_script04.b
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script05.b
+tail -2 run_script05.b
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script06.b
+tail -2 run_script06.b
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script07.b
+tail -2 run_script07.b
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script08.b
+tail -2 run_script08.b
+sed -i -e 's/LITHOSP/spc\/blessed\/bin\/lithosp/g' run_script09.b
+tail -2 run_script09.b
+sed -i -e 's/LITHOS/spc\/blessed\/bin\/lithos/g' run_script.b
+tail -4 run_script.b
