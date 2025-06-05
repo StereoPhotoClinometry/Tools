3a25087750b81761708bf7726b466c023909660b
Ally Glantzberg <allyglantzberg@gmail.com>
2025-06-05 15:06:15 -0400

    initial commit of bulkMaplets2fits

---
+++ bulkMaplet2fits.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This goes through all of the LMKs listed in the input argument
+# and turns the MAP file into a FITS file in the /MAPFILES directory.
+# The input file must be formatted like LMRKLIST (the name of the maplet
+# with no preceeding space). But, it doesn't need an end because there is
+# no maplet named END.
+#
+# Usage example:
+#   bulkMaplet2fits.sh make-maplets.txt
+# 
+# Comments added 1/14/21 by Terik Daly
+
+cd MAPFILES/
+while read line
+do
+	Maplet2Fits $line.MAP $line.FITS
+done < ../${1}
+cd ..
+
+rm SHAPEFILES/*PLT
+rm *DAT
