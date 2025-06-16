101f5ceed115533811f34f3375f0df7b3deefacd
Ally Glantzberg <allyglantzberg@gmail.com>
2025-06-16 15:14:37 -0400

    initial commit of runDisplayBatch

---
+++ runDisplayBatch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script uses a while read loop to run a batch of images
+# through Display. It converts the .pgm file written by
+# Display into a png. It saves the real images into the Display
+# directory with _R in the filename to distinguish real
+# from fake images. This script does not show a single landmark and
+# does not show limbs. It also does not change any thresholds.
+# It simply makes the images. 
+
+# Comments added 12/29/20 by Terik Daly.
+
+while read line
+do
+echo -e "${line}\n0\nn\nn" | Display
+convert TEMPFILE.pgm TEMPFILE.png
+cp TEMPFILE.png ./Display/${line}_R.png
+done <list-of-pics-to-check.txt
+
+rm ./*.DAT
+
+rm SHAPEFILES/*PLT
+rm *DAT
