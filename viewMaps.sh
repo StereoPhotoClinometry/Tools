f1efaf3d6b8f35d99f4406480707a7339bd8eb34
Ally Glantzberg <allyglantzberg@gmail.com>
2025-08-28 15:49:26 -0400

    updated viewShape to check dirs for geometry.in and open files after running

--- viewMaps.sh
+++ viewMaps.sh
@@ -28,7 +28,7 @@ list="01 02 03 04 05 06 07 08 09 10"
 
 for i in $list
 do
-	echo "~~~~Running view_$i~~~~"
+	echo "~~~~Running view_${i}~~~~"
 	if [ -f "support/geometry_$i.in" ]; then
   		cp -f support/geometry_$i.in geometry.in
 	else
