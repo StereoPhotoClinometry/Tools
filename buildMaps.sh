64aee9e927960a7390119ffe4dc8b8a9750f105b
Terik Daly <terik.daly@gmail.com>
2026-06-16 13:02:56 -0400

    replaced convert with magick

--- buildMaps.sh
+++ buildMaps.sh
@@ -1,22 +1,22 @@
-bigmap < support/bigPoleN.in; convert SIGMAS.pgm ~/send/poleN.jpg
-bigmap < support/bigPoleS.in; convert SIGMAS.pgm ~/send/poleS.jpg
+bigmap < support/bigPoleN.in; magick SIGMAS.pgm ~/send/poleN.jpg
+bigmap < support/bigPoleS.in; magick SIGMAS.pgm ~/send/poleS.jpg
 
-bigmap < support/bigEqN1.in; convert SIGMAS.pgm ~/send/1n.jpg
-bigmap < support/bigEqN2.in; convert SIGMAS.pgm ~/send/2n.jpg
-bigmap < support/bigEqN3.in; convert SIGMAS.pgm ~/send/3n.jpg
-bigmap < support/bigEqN4.in; convert SIGMAS.pgm ~/send/4n.jpg
+bigmap < support/bigEqN1.in; magick SIGMAS.pgm ~/send/1n.jpg
+bigmap < support/bigEqN2.in; magick SIGMAS.pgm ~/send/2n.jpg
+bigmap < support/bigEqN3.in; magick SIGMAS.pgm ~/send/3n.jpg
+bigmap < support/bigEqN4.in; magick SIGMAS.pgm ~/send/4n.jpg
 
-bigmap < support/bigEqS1.in; convert SIGMAS.pgm ~/send/1s.jpg
-bigmap < support/bigEqS2.in; convert SIGMAS.pgm ~/send/2s.jpg
-bigmap < support/bigEqS3.in; convert SIGMAS.pgm ~/send/3s.jpg
-bigmap < support/bigEqS4.in; convert SIGMAS.pgm ~/send/4s.jpg
+bigmap < support/bigEqS1.in; magick SIGMAS.pgm ~/send/1s.jpg
+bigmap < support/bigEqS2.in; magick SIGMAS.pgm ~/send/2s.jpg
+bigmap < support/bigEqS3.in; magick SIGMAS.pgm ~/send/3s.jpg
+bigmap < support/bigEqS4.in; magick SIGMAS.pgm ~/send/4s.jpg
 
-bigmap < support/bigEq1.in ; convert SIGMAS.pgm ~/send/1.jpg
-bigmap < support/bigEq2.in ; convert SIGMAS.pgm ~/send/2.jpg
-bigmap < support/bigEq3.in ; convert SIGMAS.pgm ~/send/3.jpg
-bigmap < support/bigEq4.in ; convert SIGMAS.pgm ~/send/4.jpg
-bigmap < support/bigEq5.in ; convert SIGMAS.pgm ~/send/5.jpg
-bigmap < support/bigEq6.in ; convert SIGMAS.pgm ~/send/6.jpg
+bigmap < support/bigEq1.in ; magick SIGMAS.pgm ~/send/1.jpg
+bigmap < support/bigEq2.in ; magick SIGMAS.pgm ~/send/2.jpg
+bigmap < support/bigEq3.in ; magick SIGMAS.pgm ~/send/3.jpg
+bigmap < support/bigEq4.in ; magick SIGMAS.pgm ~/send/4.jpg
+bigmap < support/bigEq5.in ; magick SIGMAS.pgm ~/send/5.jpg
+bigmap < support/bigEq6.in ; magick SIGMAS.pgm ~/send/6.jpg
 
-bigmap < support/biggerNPole.in; convert SIGMAS.pgm ~/send/bPoleN.jpg
-bigmap < support/biggerSPole.in; convert SIGMAS.pgm ~/send/bPoleS.jpg
+bigmap < support/biggerNPole.in; magick SIGMAS.pgm ~/send/bPoleN.jpg
+bigmap < support/biggerSPole.in; magick SIGMAS.pgm ~/send/bPoleS.jpg
