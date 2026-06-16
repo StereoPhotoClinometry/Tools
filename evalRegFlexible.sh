b8dc021c23c23cc41383c838739e36c734293f0f
Terik Daly <terik.daly@gmail.com>
2026-06-16 13:52:27 -0400

    replaced convert with magick

--- evalRegFlexible.sh
+++ evalRegFlexible.sh
@@ -124,8 +124,8 @@ do
 	echo "Running $i ($bigCnt of $total)"
 	$program < tmpRun.txt > tmpDir/$i.txt
 
-	convert TEMPFILE.ppm ./${4}/limbC-$i.jpg
-	convert TEMPFILE.pgm ./${4}/limb-$i.jpg
+	magick TEMPFILE.ppm ./${4}/limbC-$i.jpg
+	magick TEMPFILE.pgm ./${4}/limb-$i.jpg
 done
 
 
